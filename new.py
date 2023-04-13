9a55018ba574789985503a26129c0921c22a880f
MarkAndDown <49556339+MarkAndDown@users.noreply.github.com>
2023-04-13 17:50:34 +0800

    Update new.py

--- new.py
+++ new.py
@@ -1,42 +1,40 @@
 import os
 import csv
 
-# 定义要查找的文件后缀
-extensions = ['.java', '.sql', '.jsp', '.PLSQL']
+# 指定要查找的路径
+paths = ["C:/CSMS", "D:/", "E:/"]
 
-# 定义要查找的字符串
-search_string = 'PRD_18661'
+# 指定要查找的文件后缀名
+suffixes = [".java", ".sql", ".jsp", ".PLSQL"]
 
-# 定义要搜索的文件夹路径
-search_paths = ['C:/CSMS', 'D:/', 'E:/']
+# 指定要查找的字符串
+target_str = "PRD_18661"
 
-# 保存搜索结果的列表
-result = []
+# 用于存储文件名和文件路径的列表
+file_list = []
 
-# 遍历所有路径
-for path in search_paths:
-    # 遍历所有文件和文件夹
+# 递归查找符合要求的文件
+for path in paths:
     for root, dirs, files in os.walk(path):
-        # 遍历所有文件
-        for file in files:
-            # 获取文件后缀
-            extension = os.path.splitext(file)[1]
-            # 如果文件后缀符合要求
-            if extension in extensions:
-                # 拼接文件路径
-                file_path = os.path.join(root, file)
-                # 查找文件中是否含有指定字符串
-                with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
-                    contents = f.read()
-                    if search_string in contents:
-                        # 将结果加入列表中
-                        result.append((os.path.abspath(file_path), os.path.abspath(root)))
-                        
-# 去除重复项
-result = list(set(result))
+        for filename in files:
+            # 只查找指定后缀名的文件
+            if filename.endswith(tuple(suffixes)):
+                file_path = os.path.join(root, filename)
+                with open(file_path, "r", encoding="utf-8") as f:
+                    file_content = f.read()
+                    # 如果文件内容包含目标字符串，则将文件路径和文件名添加到列表中
+                    if target_str in file_content:
+                        file_list.append((filename, file_path))
 
-# 将结果保存到CSV文件中
-with open('result.csv', 'w', newline='', encoding='utf-8') as f:
+# 去重
+file_list = list(set(file_list))
+
+# 按照后缀名排序
+file_list.sort(key=lambda x: suffixes.index(os.path.splitext(x[0])[1]))
+
+# 将结果写入 csv 文件
+with open("result.csv", "w", encoding="utf-8", newline="") as f:
     writer = csv.writer(f)
-    writer.writerow(['Filename', 'Path'])
-    writer.writerows(result)
+    writer.writerow(["File Name", "File Path"])
+    for file_info in file_list:
+        writer.writerow(file_info)
