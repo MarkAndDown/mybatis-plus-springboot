0a6f55a82969a339b396cc912878f93b0ac5232b
MarkAndDown <49556339+MarkAndDown@users.noreply.github.com>
2023-04-13 18:08:43 +0800

    Update new.py

--- new.py
+++ new.py
@@ -1,39 +1,29 @@
 import os
 import csv
 
-# 多个文件夹路径
-paths = ['C:/CSMS', 'D:/', 'E:/']
-# 指定后缀名
-extensions = ['.java', '.sql', '.jsp', '.PLSQL']
-# 指定查找的字符串
-search_str = 'PRD_18661'
+file_exts = ['.java', '.sql', '.jsp', '.PLSQL']  # 需要搜索的文件后缀
+search_str = 'PRD_18661'  # 需要搜索的字符串
+search_dirs = ['C:/CSMS/back', 'D:', 'E:']  # 需要搜索的目录
 
-# 存储结果的列表
-result = []
+result = set()
 
-# 递归查找函数
-def search_files(path):
-    for file in os.listdir(path):
-        file_path = os.path.join(path, file)
-        if os.path.isdir(file_path):
-            search_files(file_path)
-        elif os.path.isfile(file_path) and any(file_path.endswith(ext) for ext in extensions):
-            with open(file_path, 'r', encoding='utf-8') as f:
-                if search_str in f.read():
-                    # 去重
-                    if file_path not in result:
-                        result.append(file_path)
+for search_dir in search_dirs:
+    for root, dirs, files in os.walk(search_dir):
+        for filename in files:
+            file_ext = os.path.splitext(filename)[1]
+            if file_ext in file_exts:
+                filepath = os.path.join(root, filename)
+                with open(filepath, 'r', encoding='utf-8', errors='ignore') as f:
+                    content = f.read()
+                    if search_str in content:
+                        result.add((filename, filepath))
 
-# 遍历路径列表
-for path in paths:
-    search_files(path)
+# 将结果按照文件后缀排序
+result = sorted(result, key=lambda x: file_exts.index(os.path.splitext(x[0])[1]))
 
-# 根据文件类型排序
-result.sort(key=lambda x: os.path.splitext(x)[1])
-
-# 写入 csv 文件
+# 将结果写入CSV文件
 with open('result.csv', 'w', newline='', encoding='utf-8') as f:
     writer = csv.writer(f)
-    writer.writerow(['文件路径', '文件名'])
-    for file_path in result:
-        writer.writerow([os.path.dirname(file_path), os.path.basename(file_path)])
+    writer.writerow(['文件名', '文件路径'])
+    for item in result:
+        writer.writerow(item)
