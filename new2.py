94e4425fb77cfcf607c54aa64ca67db936e5e646
MarkAndDown <49556339+MarkAndDown@users.noreply.github.com>
2023-04-14 00:05:16 +0800

    Update new2.py

--- new2.py
+++ new2.py
@@ -1,59 +1,120 @@
 import os
-import pandas as pd
+import re
+import xlwt
 
-# 定义需要递归查找的路径列表
-paths = ["C:/CSMS/back/fd/dss/f", "D:/", "E:/"]
-
-# 定义需要查找的文件类型
-file_types = (".java", ".sql", ".jsp", ".PLSQL")
-
-# 定义需要查找的字符串列表
-search_strings = ["PRD_18661", "PRD_18568"]
-
-# 定义输出文件名
-output_file = "output.xlsx"
-
-# 定义列名列表
-columns = ["包含的字符", "类型", "文件名", "路径", "Danny", "IFP_23_RL02_PRD_18861"]
-
-# 定义结果列表
-result = []
-
-# 递归查找文件并处理
-def search_files(path, search_strings, file_types):
-    for root, dirs, files in os.walk(path):
+# 递归查找指定文件类型的文件
+def find_files(root, extensions):
+    for subdir, dirs, files in os.walk(root):
         for file in files:
-            # 判断文件类型
-            if file.endswith(file_types):
-                # 判断文件内容是否包含搜索字符串
-                with open(os.path.join(root, file), "rb") as f:
-                    content = f.read().decode(errors="ignore")  # 忽略无法解码的字符
-                    if any(search_string in content for search_string in search_strings):
-                        # 获取文件名和文件路径
-                        file_name = os.path.basename(file)
-                        file_path = os.path.join(root, file)[len("SourceTree"):]
+            ext = os.path.splitext(file)[-1].lower()
+            if ext in extensions:
+                yield os.path.join(subdir, file)
 
-                        # 判断文件类型并设置第二列的值
-                        if file.endswith((".jsp", ".java")):
-                            col2 = "CSMS"
-                        else:
-                            col2 = "CIS"
+# 在文件中查找指定字符串
+def search_file(file_path, search_str):
+    with open(file_path, "r", encoding="utf-8") as file:
+        for i, line in enumerate(file):
+            if re.search(search_str, line):
+                return i+1  # 返回行数
+    return -1  # 没有找到
 
-                        # 添加到结果列表中
-                        result.append([search_strings[0] + "或" + search_strings[1], col2, file_name, file_path, "Danny", "IFP_23_RL02_PRD_18861"])
+# 获取文件名
+def get_file_name(file_path):
+    return os.path.basename(file_path)
 
-# 查找文件并处理
-for path in paths:
-    search_files(path, search_strings, file_types)
+# 获取文件路径去掉SourceTree之前的内容
+def get_file_path(file_path):
+    index = file_path.find("SourceTree")
+    if index >= 0:
+        return file_path[index+len("SourceTree")+1:]
+    return file_path
 
-# 将结果去重并按照文件类型排序
-df = pd.DataFrame(result, columns=columns)
-df.drop_duplicates(subset=["文件名", "路径"], keep="first", inplace=True)
-df.sort_values(by=["类型", "文件名"], ascending=True, inplace=True)
+# 生成Excel文件
+def generate_excel(results):
+    workbook = xlwt.Workbook(encoding="utf-8")
+    sheet = workbook.add_sheet("Result")
 
-# 将结果写入Excel表格
-df.to_excel(output_file, index_label="序号", startrow=1)
-# 写入包含的字符到第一行
-with pd.ExcelWriter(output_file, mode='a') as writer:
-    df_char = pd.DataFrame({"包含的字符": [search_strings[0] + "或" + search_strings[1]]})
-    df_char.to_excel(writer, index_label="序号")
+    # 写入表头
+    sheet.write(0, 0, "ID")
+    sheet.write(0, 1, "Type")
+    sheet.write(0, 2, "File Name")
+    sheet.write(0, 3, "File Path")
+    sheet.write(0, 4, "Danny")
+    sheet.write(0, 5, "Search String")
+
+    # 写入数据
+    for i, result in enumerate(results):
+
+        sheet.write(i+1, 1, "CSMS" if result["Type"] in [".jsp", ".java"] else "CIS")
+        sheet.write(i+1, 2, result["File Name"])
+        sheet.write(i+1, 3, result["File Path"])
+        sheet.write(i+1, 4, "Danny")
+        sheet.write(i+1, 5, result["Search String"])
+
+    workbook.save("result.xls")
+
+# 搜索指定字符串并输出结果到Excel表格
+def search_and_output(paths, extensions, search_strs):
+    results = []
+    file_names = set()  # 用于去重
+
+    for search_str in search_strs:
+        # 按照文件类型排序
+        result = {
+            "Type": "",
+            "File Name": "",
+            "File Path": "",
+            "Search String": search_str
+        }
+        results.append(result)
+        for path in paths:
+            for file_path in find_files(path, extensions):
+                file_name = get_file_name(file_path)
+                if (search_str,file_name,path) in file_names:  # 如果已经处理过该文件，则跳过
+                    continue
+                file_names.add(file_name)
+                line_num = search_file(file_path, search_str)
+                if line_num >= 0:
+                    result = {
+                        "Type": os.path.splitext(file_path)[-1],
+                        "File Name": file_name,
+                        "File Path": get_file_path(file_path),
+                        "Search String": search_str
+                    }
+                    results.append(result)
+
+
+    # 生成Excel文件
+    workbook = xlwt.Workbook(encoding="utf-8")
+    sheet = workbook.add_sheet("Result")
+    # 写入表头
+    sheet.write(0, 0, "ID")
+    sheet.write(0, 1, "Type")
+    sheet.write(0, 2, "File Name")
+    sheet.write(0, 3, "File Path")
+    sheet.write(0, 4, "Danny")
+    sheet.write(0, 5, "Search String")
+    # 写入数据
+    for i, result1 in enumerate(results):
+        if (len(result1["File Name"])==0 and len(result1["File Path"])==0):
+            sheet.write_merge(i + 1, i + 1, 0, 5, result["Search String"])
+            continue
+        sheet.write(i + 1, 0, i + 1)
+        sheet.write(i + 1, 1, "CSMS" if result1["Type"] in [".jsp", ".java"] else "CIS")
+        sheet.write(i + 1, 2, result1["File Name"])
+        sheet.write(i + 1, 3, result1["File Path"])
+        sheet.write(i + 1, 4, "Danny")
+        sheet.write(i + 1, 5, result1["Search String"])
+
+    workbook.save("result.xls")
+
+# 测试代码
+
+
+if __name__ == "__main__":
+    paths = ["E:/test/SourceTree/cca", "E:/test/SourceTree/fd"]
+    extensions = [".java", ".sql", ".jsp", ".PLSQL"]
+    # 定义要查找的字符串
+    search_strs = ["PRD_18661", "PRD_18568"]
+
+    search_and_output(paths, extensions,search_strs)
