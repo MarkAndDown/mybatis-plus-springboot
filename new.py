d836ed7d768cf7c31d5fb106fa54e018ef46a322
MarkAndDown <49556339+MarkAndDown@users.noreply.github.com>
2023-04-13 18:57:38 +0800

    Update new.py

--- new.py
+++ new.py
@@ -1,57 +1,71 @@
 import os
 import csv
 
-# 定义要查找的路径
+# 定义需要查找的路径
 paths = ["C:/CSMS/back/fd/dss/f", "D:/", "E:/"]
 
-# 定义要查找的文件类型和要查找的字符串
-extensions = ['.java', '.sql', '.jsp', '.PLSQL']
-search_str = "PRD_18661"
+# 定义需要查找的文件后缀和关键字
+file_exts = ['.java', '.sql', '.jsp', '.PLSQL']
+search_str = 'PRD_18661'
 
-# 定义要输出的csv文件名和列名
-csv_filename = "output.csv"
-csv_columns = ['序号', '展示', '文件名', '文件路径', 'Danny', 'IFP_23_RL02_PRD_18861']
+# 定义需要输出的文件名
+output_filename = 'output.csv'
 
-# 初始化结果列表
-result = []
+# 定义需要输出的列名
+fieldnames = ['Index', 'Project', 'Filename', 'Path', 'Author', 'Keyword']
 
-# 递归函数查找指定文件类型的文件并判断是否包含指定字符串
-def find_files(path):
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            # 只处理指定后缀名的文件
-            if file.endswith(tuple(extensions)):
-                with open(os.path.join(root, file), 'r', encoding='utf-8') as f:
-                    # 判断文件内容是否包含指定字符串
-                    if search_str in f.read():
-                        # 获取文件的完整路径和文件名
-                        full_path = os.path.join(root, file)
-                        filename = os.path.basename(full_path)
-                        # 获取文件路径中去掉SourceTree之前的部分
-                        path_parts = full_path.split("\\")
-                        idx = path_parts.index("SourceTree")
-                        file_path = "\\".join(path_parts[idx+1:])
-                        # 判断文件类型，确定展示类型
-                        if file.endswith('.java') or file.endswith('.jsp'):
-                            display_type = 'CSMS'
+# 定义用于去重的集合
+unique_files = set()
+
+# 定义计数器
+count = 0
+
+# 定义项目名称字典
+project_dict = {
+    '.java': 'CSMS',
+    '.jsp': 'CSMS',
+    '.sql': 'CIS',
+    '.PLSQL': 'CIS'
+}
+
+# 定义路径中需要删除的部分
+path_to_remove = 'SourceTree'
+
+# 定义作者名
+author_name = 'Danny'
+
+# 定义关键字
+keyword = 'IFP_23_RL02_PRD_18861'
+
+# 定义输出文件的打开方式
+with open(output_filename, mode='w', newline='') as output_file:
+    writer = csv.DictWriter(output_file, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for path in paths:
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if any(file.endswith(ext) for ext in file_exts):
+                    file_path = os.path.join(root, file)
+                    if search_str in open(file_path, encoding="utf8").read():
+                        # 获取文件名和后缀
+                        filename, ext = os.path.splitext(file)
+                        # 获取项目名称
+                        project = project_dict.get(ext, '')
+                        # 去除路径中的部分
+                        path = root.split(path_to_remove, 1)[-1]
+                        # 如果路径已存在则跳过
+                        if (filename, path) in unique_files:
+                            continue
                         else:
-                            display_type = 'CIS'
-                        # 添加结果到结果列表中
-                        result.append([len(result)+1, display_type, filename, file_path, 'Danny', 'IFP_23_RL02_PRD_18861'])
-
-# 遍历所有路径
-for path in paths:
-    find_files(path)
-
-# 去重
-result = list(set(tuple(row) for row in result))
-
-# 按照文件类型排序
-result.sort(key=lambda x: extensions.index(os.path.splitext(x[2])[1]))
-
-# 写入csv文件
-with open(csv_filename, 'w', newline='', encoding='utf-8') as f:
-    writer = csv.writer(f)
-    writer.writerow(csv_columns)
-    for row in result:
-        writer.writerow(row)
+                            unique_files.add((filename, path))
+                        # 写入csv文件
+                        count += 1
+                        writer.writerow({
+                            'Index': count,
+                            'Project': project,
+                            'Filename': filename,
+                            'Path': path,
+                            'Author': author_name,
+                            'Keyword': keyword
+                        })
