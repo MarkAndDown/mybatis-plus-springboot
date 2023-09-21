d69907c7bdd367c7ba534e1060f220eeddc90bfa
MarkAndDown <49556339+MarkAndDown@users.noreply.github.com>
2023-09-22 01:44:27 +0800

    Update new2.py

--- new2.py
+++ new2.py
@@ -11,8 +11,8 @@ repo = git.Repo(repo_path)
 # 计算一个月前的日期
 one_month_ago = datetime.now() - timedelta(days=30)
 
-# 用于存储匹配的提交记录和文件
-matching_commits = []
+# 用于存储匹配的文件的集合
+matching_files_set = set()
 
 # 遍历最近一个月的提交记录
 for commit in repo.iter_commits(since=one_month_ago):
@@ -20,36 +20,56 @@ for commit in repo.iter_commits(since=one_month_ago):
 
     # 检查提交备注是否包含 "PPP-111"
     if "ppp-111" in commit_message:
-        matching_files = set()
-
         # 遍历提交的文件差异
         for diff in commit.diff():
             # 获取文件的路径
             file_path = diff.a_path or diff.b_path
 
-            # 打开文件并逐行检查是否包含 "DDD_222"
-            with open(file_path, 'r', encoding='utf-8') as file_content:
-                lines = file_content.readlines()
-                for line in lines:
-                    if re.search(r'ddd_222', line, re.IGNORECASE):
-                        matching_files.add(file_path)
-                        break  # 如果文件中包含一行即可满足条件，可以提前结束对该文件的检查
+            try:
+                # 打开文件并逐行检查是否包含 "DDD_222"
+                with open(file_path, 'r', encoding='utf-8') as file_content:
+                    lines = file_content.readlines()
+                    for line in lines:
+                        if re.search(r'ddd_222', line, re.IGNORECASE):
+                            matching_files_set.add(file_path)
+                            break  # 如果文件中包含一行即可满足条件，可以提前结束对该文件的检查
+            except FileNotFoundError:
+                # 如果无法打开文件，跳过该文件的处理
+                pass
 
-        # 如果匹配的文件不为空，则将提交记录添加到匹配的提交记录列表中
-        if matching_files:
-            matching_commits.append({
-                "commit": commit,
-                "matching_files": matching_files
-            })
+# 定义不同类别的文件字典
+file_categories = {
+    "rule": [],
+    "constant": [],
+    "util": [],
+    "mapper": [],
+    "domain": [],
+    "vo": [],
+    "dml": [],
+    "test": []
+}
 
-# 打印匹配的提交记录和对应的文件
-for match in matching_commits:
-    print(f"Commit SHA: {match['commit'].hexsha}")
-    print(f"Commit Message: {match['commit'].message}")
-    print("Matching Files:")
-    
-    # 遍历匹配的文件
-    for file in match['matching_files']:
+# 遍历匹配的文件列表，并根据文件路径中的字符分类
+for file_path in matching_files_set:
+    if "rule" in file_path.lower():
+        file_categories["rule"].append(file_path)
+    elif "constant" in file_path.lower():
+        file_categories["constant"].append(file_path)
+    elif "util" in file_path.lower():
+        file_categories["util"].append(file_path)
+    elif "mapper" in file_path.lower():
+        file_categories["mapper"].append(file_path)
+    elif "domain" in file_path.lower():
+        file_categories["domain"].append(file_path)
+    elif "vo" in file_path.lower():
+        file_categories["vo"].append(file_path)
+    elif "dml" in file_path.lower():
+        file_categories["dml"].append(file_path)
+    elif "test" in file_path.lower():
+        file_categories["test"].append(file_path)
+
+# 打印分类后的文件列表
+for category, files in file_categories.items():
+    print(f"{category.capitalize()} Files:")
+    for file in files:
         print(f"  - {file}")
-
-    print()
