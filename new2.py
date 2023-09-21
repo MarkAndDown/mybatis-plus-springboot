2a220cc6a507e7802c87c12553922021f6094d11
MarkAndDown <49556339+MarkAndDown@users.noreply.github.com>
2023-09-22 01:30:09 +0800

    Update new2.py

--- new2.py
+++ new2.py
@@ -11,7 +11,7 @@ repo = git.Repo(repo_path)
 # 计算一个月前的日期
 one_month_ago = datetime.now() - timedelta(days=30)
 
-# 用于存储匹配的提交记录
+# 用于存储匹配的提交记录和文件
 matching_commits = []
 
 # 遍历最近一个月的提交记录
@@ -27,9 +27,13 @@ for commit in repo.iter_commits(since=one_month_ago):
             # 获取文件的路径
             file_path = diff.a_path or diff.b_path
 
-            # 检查文件是否包含 "DDD_222"
-            if re.search(r'ddd_222', diff.diff.decode('utf-8'), re.IGNORECASE):
-                matching_files.add(file_path)
+            # 打开文件并逐行检查是否包含 "DDD_222"
+            with open(file_path, 'r', encoding='utf-8') as file_content:
+                lines = file_content.readlines()
+                for line in lines:
+                    if re.search(r'ddd_222', line, re.IGNORECASE):
+                        matching_files.add(file_path)
+                        break  # 如果文件中包含一行即可满足条件，可以提前结束对该文件的检查
 
         # 如果匹配的文件不为空，则将提交记录添加到匹配的提交记录列表中
         if matching_files:
@@ -43,6 +47,9 @@ for match in matching_commits:
     print(f"Commit SHA: {match['commit'].hexsha}")
     print(f"Commit Message: {match['commit'].message}")
     print("Matching Files:")
+    
+    # 遍历匹配的文件
     for file in match['matching_files']:
         print(f"  - {file}")
+
     print()
